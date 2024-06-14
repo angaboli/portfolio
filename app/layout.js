5710cc6ed350d4895efd4a5e4eb818e605b4da85
Ngabonziza Aimé <nzizaba@gmail.com>
2024-06-14 16:51:28 +0200

    gtm

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter, Jost } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import NavBar from '@/components/NavBar'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Jost({ subsets: ["latin"] });
 
@@ -13,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning className="scroll-smooth focus:scroll-auto">
+      <GoogleTagManager gtmId="GTM-NSPZ3VTH" />
       <body className={`${inter.className} `}>
         <Providers>
           {children}
